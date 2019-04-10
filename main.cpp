fa82e0d30ce220be6534a933fb80af49b3ca59e5
Aviad Pinis <aviadpinis@gmail.com>
2019-04-10 14:50:23 +0300

    Delete unnecessary functions

--- main.cpp
+++ main.cpp
@@ -7,59 +7,31 @@
 
 using namespace std;
 
-bool is_not_digit(char c)
-{
-    return !std::isdigit(c);
-}
-
-bool compareNumbers(string str1, string str2)
-{
-    // Calculate lengths of both string
-    int n1 = str1.length(), n2 = str2.length();
-
-    if (n1 < n2)
-        return true;
-    if (n2 < n1)
-        return false;
-
-    // If lengths are same
-    for (int i=0; i<n1; i++)
-    {
-        if (str1[i] < str2[i])
-            return true;
-        if (str1[i] > str2[i])
-            return false;
-    }
-
-    return false;
-}
-
-int splitString(const std::string& s1){
-    for (int i=0;i<s1.length();i++){
-        if(std::isdigit(s1[i])){
+int splitString(const std::string &s1) {
+    for (int i = 0; i < s1.length(); i++) {
+        if (std::isdigit(s1[i])) {
             return i;
         }
     }
 }
 
-bool numeric_string_compare(const std::string& s1, const std::string& s2)
-{
+bool numeric_string_compare(const std::string &s1, const std::string &s2) {
     int indexToSplitS1 = splitString(s1);
     int indexToSplitS2 = splitString(s2);
     string nameS1 = s1.substr(indexToSplitS1);
     string nameS2 = s2.substr(indexToSplitS2);
     int found = s1.find(".");
-    if( nameS1.compare(nameS2)){
-        int numS1 = atoi(s1.substr(indexToSplitS1,s1.find(".")-indexToSplitS1).c_str());
-        int numS2 = atoi(s2.substr(indexToSplitS2,s2.find(".")-indexToSplitS2).c_str());
-        return numS1<numS2;
-    } else{
+    if (nameS1.compare(nameS2)) {
+        int numS1 = atoi(s1.substr(indexToSplitS1, s1.find(".") - indexToSplitS1).c_str());
+        int numS2 = atoi(s2.substr(indexToSplitS2, s2.find(".") - indexToSplitS2).c_str());
+        return numS1 < numS2;
+    } else {
         std::string::const_iterator it1 = s1.begin(), it2 = s2.begin();
         return std::lexicographical_compare(it1, s1.end(), it2, s2.end());
     }
 }
 
-vector<std::string> getListImageNameFromPathFolder(string namePath){
+vector<std::string> getListImageNameFromPathFolder(string namePath) {
     struct dirent *de; // Pointer for directory entry
 
     // opendir() returns a pointer of DIR type.
@@ -67,7 +39,7 @@ vector<std::string> getListImageNameFromPathFolder(string namePath){
 
     if (dr == NULL) // opendir returns NULL if couldn't open directory
     {
-        printf("Could not open current directory" );
+        printf("Could not open current directory");
         return {};
     }
 
@@ -75,7 +47,7 @@ vector<std::string> getListImageNameFromPathFolder(string namePath){
     std::vector<std::string> imagesName;
     while ((de = readdir(dr)) != NULL) {
         string name = de->d_name;
-        if(name.find(".jpg") != string::npos) {
+        if (name.find(".jpg") != string::npos) {
             imagesName.push_back(de->d_name);
         }
     }
@@ -88,13 +60,12 @@ vector<std::string> getListImageNameFromPathFolder(string namePath){
     return imagesName;
 }
 
-int main(void)
-{
-    string pathName= "../../../Desktop/src/data/Images/odo360nodoor/odo360nodoor_segmentation/";
+int main(void) {
+    string pathName = "../../../Desktop/src/data/Images/odo360nodoor/odo360nodoor_segmentation/";
     std::vector<std::string> imagesName = getListImageNameFromPathFolder(pathName);
 
-    for(int i=0;i<imagesName.size();i++){
-        cout<<imagesName[i]<<endl;
+    for (int i = 0; i < imagesName.size(); i++) {
+        cout << imagesName[i] << endl;
     }
     return 0;
 }
